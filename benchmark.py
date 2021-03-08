532a1e914dcb15a29cfe12d4b23aec9baffee369
Antoine CLEMENT <clementoine@hotmail.fr>
2021-03-08 09:57:42 +0100

    update benchmark

--- benchmark.py
+++ benchmark.py
@@ -8,18 +8,17 @@ from itertools import count
 import pandas as pd
 import random
 from sqlalchemy.sql import select
-from tools import benchmark,benchmark_thread
+from tools import benchmark, benchmark_thread
 import redis
-
 import matplotlib.pyplot as plt
-
 
 # --- SQL SETTINGS ---
 
 POSTGRES_PASSWORD = 'no_pass'
 PORT = '5432'
 
-engine = create_engine(f"postgresql://postgres:{POSTGRES_PASSWORD}@localhost:{PORT}/postgres",pool_size=50, max_overflow=20)
+engine = create_engine(f"postgresql://postgres:{POSTGRES_PASSWORD}@localhost:{PORT}/postgres", pool_size=50,
+                       max_overflow=20)
 
 base = declarative_base()
 metadata = MetaData(engine)
@@ -39,7 +38,7 @@ netflix_movies = Table(
     Column('cast', Text, nullable=False),
     Column('country', Text, nullable=False),
     Column('date_added', Text, nullable=False),
-    Column('release_year', Text, nullable=False),
+    Column('release_year', Integer, nullable=False),
     Column('rating', Text, nullable=False),
     Column('duration', Text, nullable=False),
     Column('listed_in', Text, nullable=False),
@@ -107,8 +106,8 @@ def selectRandom():
     conn.execute(s)
 
 
-#benchmark({'redis same line': hgetall, 'redis random line': hgetallrandom, 'psql same line': selectFixed,
-           #'psql random line': selectRandom})
+benchmark({'redis same line': hgetall, 'redis random line': hgetallrandom, 'psql same line': selectFixed,
+           'psql random line': selectRandom})
 
 
 # ----- GET SHOW ID----#
@@ -122,7 +121,7 @@ def selectShowId4000():
     conn.execute(s)
 
 
-#benchmark({'redis s4000': hmget4000, 'psql s4000': selectShowId4000})
+benchmark({'redis s4000': hmget4000, 'psql s4000': selectShowId4000})
 
 indice_redis = count(start=8000)
 
@@ -139,22 +138,13 @@ def insertPsql():
     idx = next(indice_psql)
     i = insert(netflix_movies).values(show_id='s' + str(idx), type='type', title='title', director='director',
                                       cast='cast', country='country', date_added='date_added',
-                                      release_year='release_year', rating='rating', duration='duration',
+                                      release_year=2014, rating='rating', duration='duration',
                                       listed_in='listed_in', description='description')
     conn.execute(i)
 
 
 # ----- SET A LINE----#
-#benchmark({'redis insert': hmset, 'psql insert': insertPsql})
-
-# ----- KEY exists?---#
-
-def exist7000():
-    r.exists('s7000')
-
-
-# benchmark({'s1': exist1, 's2': exist7000})
-
+benchmark({'redis insert': hmset, 'psql insert': insertPsql})
 
 
 def sql_director_exists_head():
@@ -178,114 +168,136 @@ def sql_director_exists_end():
     conn.execute(req)
 
 
-
 def redis_director_exists_head():
     for show_id in r.scan_iter():
-        if (r.hget(show_id,'director')==b'David Raynr'):
+        if r.hget(show_id, 'director') == b'David Raynr':
             return
 
 
 def redis_director_exists_middle():
     for show_id in r.scan_iter():
-        if (r.hget(show_id, 'director') == b'Lance Daly'):
+        if r.hget(show_id, 'director') == b'Lance Daly':
             return True
     return False
 
 
 def redis_director_exists_end():
     for show_id in r.scan_iter():
-        if (r.hget(show_id, 'director') == b'Emma Hatherley'):
+        if r.hget(show_id, 'director') == b'Emma Hatherley':
             return True
     return False
 
 
-"""benchmark({'redis dir exists head': redis_director_exists_head, 'redis dir exists middle': redis_director_exists_middle,
-           'redis dir exists end': redis_director_exists_end, 'psql dir exists head': sql_director_exists_head,
-           'psql dir exists middle': sql_director_exists_middle, 'psql dir exists end': sql_director_exists_end}, run_nb=10)"""
-
-
-def sql_indian_films():
-    req = 'SELECT show_id FROM netflix_movies WHERE country = \'India\';'
-    conn.execute(req)
+benchmark({'redis dir exists head': redis_director_exists_head,
+           'redis dir exists middle': redis_director_exists_end, 'redis dir exists end': redis_director_exists_middle,
+           'psql dir exists head': sql_director_exists_head,
+           'psql dir exists middle': sql_director_exists_middle, 'psql dir exists end': sql_director_exists_end},
+          run_nb=100)
 
 
 def redis_indian_films():
     indian_films = []
     for show_id in r.scan_iter():
-        if (r.hget(show_id, 'country') == b'India'):
+        if r.hget(show_id, 'country') == b'India':
             indian_films.append(show_id)
 
-#benchmark({'redis indian films': redis_director_exists_head, 'psql indian films': redis_director_exists_middle}, run_nb=10)
+
+def psql_indian_films():
+    req = 'SELECT show_id FROM netflix_movies WHERE country = \'India\';'
+    conn.execute(req)
+
+
+benchmark({'redis indian films': redis_indian_films, 'psql indian films': psql_indian_films}, run_nb=10)
+
 
 def increment():
     r.hincrby("s7001", "release_year")
 
+
 def update():
-    req='UPDATE netflix_movies\
+    req = 'UPDATE netflix_movies\
         SET release_year = CAST(release_year AS INTEGER) + 1\
         WHERE show_id = \'s7001\''
     conn.execute(req)
 
 
-#benchmark({'redis incr n': increment,'psql incr': update},run_nb=10000)
+benchmark({'redis incr n': increment, 'psql incr': update}, run_nb=10000)
 
 
+def redis_compute_mean():
+    _sum = 0
+    _count = 0
+    for show_id in r.scan_iter():
+        _count += 1
+        _sum += int(r.hget(show_id, 'release_year'))
+    return _sum / _count
 
 
-############THREADING######################
+def psql_compute_mean():
+    req = 'SELECT avg(release_year) from netflix_movies;'
+    conn.execute(req)
+    return req
+
+
+benchmark({'redis avg': redis_compute_mean, 'psql avg': psql_compute_mean}, run_nb=100)
+
+
+# -----------THREADING------------#
 
 # ------- GET A WHOLE LINE -------#
 
-def hgetall(r):
+def hgetall():
     r.hgetall('s7392')
 
 
-def selectFixed(conn):
+def selectFixed():
     s = select([netflix_movies]).where(netflix_movies.c.show_id == 's7392')
     conn.execute(s)
 
 
-#benchmark_thread({'redis same line': (hgetall,'r'),  'psql same line': (selectFixed,'psql')},engine,1000,50,True)
+benchmark_thread({'redis same line': (hgetall, 'r'), 'psql same line': (selectFixed, 'psql')}, engine, 1000, 50, True)
+
 
 # ----- GET SHOW ID----#
 
-def hmget4001(r):
+def hmget4001():
     r.hmget('s4001', 'show_id')
 
 
-def selectShowId4001(conn):
+def selectShowId4001():
     s = select([netflix_movies.c.show_id]).where(netflix_movies.c.show_id == 's4001')
     conn.execute(s)
 
-#benchmark_thread({'redis hmget': (hgetall,'r'),  'psql select show id': (selectFixed,'psql')},engine,1000,50,True)
+
+benchmark_thread({'redis hmget': (hgetall, 'r'), 'psql select show id': (selectFixed, 'psql')}, engine, 1000, 50, True)
+
 
 # ----- Incremetnation----#
 
-def increment(r):
+def increment():
     r.hincrby("s7001", "release_year")
 
-def update(conn):
-    req='UPDATE netflix_movies\
-        SET release_year = CAST(release_year AS INTEGER) + 1\
+
+def update():
+    req = 'UPDATE netflix_movies\
+        SET release_year = release_year + 1\
         WHERE show_id = \'s7001\''
     conn.execute(req)
 
-medians_redis=[]
-medians_psql=[]
-index=[]
-for i in range(10):
+
+medians_redis = []
+medians_psql = []
+index = []
+for j in range(10):
     print("STEP")
-    median_redis,median_psql=benchmark_thread({'redis incr': (increment,'r'),'psql incr': (update,'sql')},engine,1000,i*5+1,False)
+    median_redis, median_psql = benchmark_thread({'redis incr': (increment, 'r'), 'psql incr': (update, 'sql')}, engine,
+                                                 1000, j * 5 + 1, False)
     medians_redis.append(median_redis)
     medians_psql.append(median_psql)
-    index.append(i*5+1)
-print(index,medians_psql,medians_psql)
-plt.plot(index,medians_redis,label='redis')
-plt.plot(index,medians_psql,label='psql')
+    index.append(j * 5 + 1)
+print(index, medians_psql, medians_psql)
+plt.plot(index, medians_redis, label='redis')
+plt.plot(index, medians_psql, label='psql')
 plt.legend()
 plt.show()
 
-
-
-
-#print(r.hget("s7001",'release_year'))
